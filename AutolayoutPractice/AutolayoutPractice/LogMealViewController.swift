1feb197fe8195ff34600c35e2fe0f4a708edaffe
Nogah Melamed Cohen <nogah@cherrypick-consulting.com>
2019-10-29 12:08:14 +0200

    custom cell layout fixed

--- AutolayoutPractice/AutolayoutPractice/LogMealViewController.swift
+++ AutolayoutPractice/AutolayoutPractice/LogMealViewController.swift
@@ -8,26 +8,45 @@
 
 import UIKit
 
-class LogMealViewController: UIViewController {
-
-//    var dataSource: UITableViewDataSource
-//    var delegate: UITableViewDelegate = self
-//    
+class LogMealViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Register the table view cell class and its reuse id
+        self.foodTableView.register(LogMealTableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
         // Do any additional setup after loading the view.
+        self.foodTableView.delegate = self
+        self.foodTableView.dataSource = self
+
     }
     
+    @IBOutlet weak var foodTableView: UITableView!
+    var foodNames: [String] = ["apple", "bread"]
+    var foodDescription: [String] = ["",""]
+    var foodPicture: [UIImage?] = [nil,nil]
+    let cellReuseIdentifier = "cell"
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.foodNames.count
     }
-    */
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create a new cell if needed or reuse an old one
+        let cell: LogMealTableViewCell = (self.foodTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! LogMealTableViewCell?)!
+
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.foodNames[indexPath.row]
+        
+        return cell
+    }
+
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
 
 }
